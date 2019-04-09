e30617c4b94feb1086094d3dcd98b80b49e892cb
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-09 19:46:38 -0400

    generated User

---
+++ lib/expense_tracker_web/views/user_view.ex
@@ -0,0 +1,18 @@
+defmodule ExpenseTrackerWeb.UserView do
+  use ExpenseTrackerWeb, :view
+  alias ExpenseTrackerWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      email: user.email,
+      encrypted_password: user.encrypted_password}
+  end
+end
