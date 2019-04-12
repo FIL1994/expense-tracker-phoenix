848770f5e9d51b80ddb8be55706e3f28bb4965f5
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-11 22:58:40 -0400

    report coverage

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ExpenseTracker.MixProject do
       {:guardian, "~> 1.0"},
       {:comeonin, "~> 4.0"},
       {:bcrypt_elixir, "~> 1.0"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:excoveralls, "~> 0.10.6"}
     ]
   end
 
